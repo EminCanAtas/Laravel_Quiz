dcfc9fdfadbf4d5a0dd4cfbe80e0a719e6c26b21
Emin Can Ataş <ghost_recon_jaga@hotmail.com>
2021-02-01 00:07:04 +0300

    Migration - Seed - Factory oluşturuldu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,17 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
-
-        \App\Models\User::insert
+        $this->call
         ([
-            'name' => 'Emin Can Ataş',
-            'email' => 'ghost_recon_jaga@hotmail.com',
-            'email_verified_at' => now(),
-            'type'=> 'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            UserSeeder::class,
+            QuizSeeder::class
         ]);
-        
     }
 }
