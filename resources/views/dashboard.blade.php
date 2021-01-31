1b79ed81661ca7b69533dfd82e122b317a1528e4
Emin Can Ataş <ghost_recon_jaga@hotmail.com>
2021-01-31 22:39:11 +0300

    Jetstream ve Bootstrap kuruldu Page heading ve content değiştirildi

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">Anasayfa</x-slot>
+
+    <div class="alert alert-danger">Bootstrap Alert Mesajı</div>
+    
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <x-jet-welcome />
+    </div>
+</x-app-layout>
